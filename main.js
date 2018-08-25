1c6fe4ee3b3b159725df70e308e01019f4d9eb84
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-08-25 10:26:48 -0500

    Object shorthand notation

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ const firstName = 'jonathan';
 const lastName = 'mohan';
 // greetingElement.innerHTML = 'Hello my name is ' + firstName + ' ' + lastName;
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+// object shorthand notation
+let name = 'jonathan';
+let age = '39';
+
+//let person = {
+   // name: name,
+ //   age: age
+//};
+
+let person = {name, age};
+console.log('person:', person.name);
